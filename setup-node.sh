8abbdf7ce306e7143fdef1d35dcf128d15adf1d4
Taha <taha.almokahel@gmail.com>
2021-12-23 02:47:38 +0300

    2 commit

--- setup-node.sh
+++ setup-node.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+docker pull node:16-alpine
 docker pull 121180073/dis-node
 docker pull fluree/ledger
 docker pull openwhisk/standalone:nightly
 
-docker run --rm -p 50007:50007 -e TYPE=master -e HOST=192.168.1.70 -v /var/run/docker.sock:/var/run/docker.sock --name dis-node --privileged dis-node:latest
+docker run --rm -p 50007:50007 -e TYPE=master -e HOST=192.168.1.70 -v /var/run/docker.sock:/var/run/docker.sock --name dis-node --privileged 121180073/dis-node:latest
