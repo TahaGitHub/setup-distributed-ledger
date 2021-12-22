681200c7a1b4530520b0f09642a32f78d882be07
Taha <taha.almokahel@gmail.com>
2021-12-23 01:39:27 +0300

    2 commit

--- setup-node.sh
+++ setup-node.sh
@@ -1,26 +1,7 @@
 #!/bin/sh
 
-if [ $1 = 'pc' ]
-then
-# echo Hey $1.
-apt update
-apt install --yes software-properties-common
-add-apt-repository --yes --update ppa:ansible/ansible
-apt install --yes ansible
+docker pull 121180073/dis-node
+docker pull fluree/ledger
+docker pull openwhisk/standalone:nightly
 
-ansible-playbook install-docker-kubernetes.yaml -k -K
-
-sudo usermod -aG docker $USER && newgrp docker
-fi
-
-[ $1 = 'reader' ]
-then
-echo Hey $1
-fi
-
-sudo kubeadm init --apiserver-advertise-address=192.168.1.70 --pod-network-cidr=192.168.0.0/16
-sudo iptables -F && sudo iptables -t nat -F && sudo iptables -t mangle -F && sudo iptables -X
-
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+docker run --rm -p 50007:50007 -e TYPE=master -e HOST=192.168.1.70 -v /var/run/docker.sock:/var/run/docker.sock --name dis-node --privileged dis-node:latest
